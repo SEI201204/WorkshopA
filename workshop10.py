6c4774433ab871c4537bc3d13f562a7a7c1cf5ce
SEI201204 <s6519410036@sau.ac.th>
2023-09-27 15:57:25 +0700

    code v2 : 4-13

--- workshop10.py
+++ workshop10.py
@@ -3,4 +3,34 @@
 # กรณีป้อน 2 แสดงข้อความ "Welcome Sophomore"
 # กรณีป้อน 3 แสดงข้อความ "Welcome Junior"
 # กรณีป้อน 4 แสดงข้อความ "Welcome Senior"
+#1.รับค่าข้อมูลนักศึกษาทางแป้นพิมพ์ 2.ตรวจสอบข้อมูล 3.แสดงผลทางหน้าจอ 4.แสดงชื่อโปรแกรมหน้าจอ
 
+def inputData( ) :
+    years = int(input('โปรดใส่ชั้นปีของคุณ (1-4): '))
+    return years
+
+def cheakYear(years) :
+    if years == 1 :
+        return('Welcome Freshman')
+    elif years == 2 :
+        return('Welcome Sophomore')
+    elif years == 3 :
+        return('Welcome Junior')
+    elif years == 4 :
+        return('Welcome Senior')
+    else :
+        return('ไม่รู้จักชั้นปีที่ระบุ')
+    
+def showCheakYear(years) :
+    print(f'ชั้นปีของคุณคือ ปี{years}')
+    print(cheakYear(years))
+    print('**--------------------------------**')
+
+def showProgramName( ) :
+    print('**--------------------------------**')
+    print('**------โปรแกรมต้อนรับนักศึกษา-------**')
+    print('**--------------------------------**')
+
+years=inputData( )
+showProgramName( )
+showCheakYear(years)
